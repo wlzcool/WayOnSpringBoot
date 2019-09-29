ae5d2216dab4fed4978ed437cea1a94b4288f344
wlzcool <wlzcool@foxmail.com>
2019-09-29 17:08:49 +0800

    建立项目

---
+++ springbootfirststep/src/main/java/com/wayfather/springbootfirststep/SpringbootfirststepApplication.java
@@ -0,0 +1,13 @@
+package com.wayfather.springbootfirststep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfirststepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootfirststepApplication.class, args);
+    }
+
+}
