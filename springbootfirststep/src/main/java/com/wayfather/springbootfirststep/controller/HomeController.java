e35d14b15a7869c99dfbcbb07eae03ec1f4b5525
wlzcool <wlzcool@foxmail.com>
2019-09-30 08:14:32 +0800

    添加类注释

---
+++ springbootfirststep/src/main/java/com/wayfather/springbootfirststep/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.wayfather.springbootfirststep.controller;
+
+/**
+ * @ClassName HomeController
+ * @Description TODO
+ * @Author IBM
+ * @Date 2019/9/29 17:36
+ **/
+public class HomeController {
+}
