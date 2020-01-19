b8d710c5ffdb35bfece388c1ca65b1a8a7d54491
wlzcool <wlzcool@foxmail.com>
2020-01-19 14:22:07 +0800

    修改bug

--- springboot-easypoi/src/main/java/com/wayfather/springbooteasypoi/controller/ExportController.java
+++ springboot-easypoi/src/main/java/com/wayfather/springbooteasypoi/controller/ExportController.java
@@ -2,8 +2,13 @@ package com.wayfather.springbooteasypoi.controller;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
+import cn.hutool.core.util.RandomUtil;
 import com.wayfather.springbooteasypoi.common.DownloadWriter;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +19,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
  * @author IBM
@@ -29,48 +33,66 @@ import java.util.Map;
 @Controller
 @RequestMapping("export")
 public class ExportController {
+    @Autowired
+    ResourceLoader resourceLoader;
     @RequestMapping("easypoi")
-    public void exportEasyPOI(HttpServletRequest request, HttpServletResponse response){
+    public void exportEasyPOI(HttpServletRequest request, HttpServletResponse response) {
+        Resource resource = resourceLoader.getResource("classpath:xlstemplate/businessTemplate.xlsx");
+//        TemplateExportParams params = new TemplateExportParams(
+//                "src/main/resources/xlstemplate/businessTemplate.xlsx");
+        System.out.println(((ClassPathResource) resource).getPath());
         TemplateExportParams params = new TemplateExportParams(
-                "src/main/resources/xlstemplate/businessTemplate.xlsx");
+                ((ClassPathResource) resource).getPath());
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("businessNumber", "1910221439386584");
-//        map.put("money", 2000000.00);
-//        map.put("upperMoney", "贰佰万");
-//        map.put("company", "执笔潜行科技有限公司");
-//        map.put("bureau", "财政局");
-//        map.put("person", "JueYue");
-//        map.put("phone", "1879740****");
-//        List<Map<String, String>> listMap = new ArrayList<Map<String, String>>();
-//        for (int i = 0; i < 4; i++) {
-//            Map<String, String> lm = new HashMap<String, String>();
-//            lm.put("id", i + 1 + "");
-//            lm.put("zijin", i * 10000 + "");
-//            lm.put("bianma", "A001");
-//            lm.put("mingcheng", "设计");
-//            lm.put("xiangmumingcheng", "EasyPoi " + i + "期");
-//            lm.put("quancheng", "开源项目");
-//            lm.put("sqje", i * 10000 + "");
-//            lm.put("hdje", i * 10000 + "");
-//
-//            listMap.add(lm);
-//        }
-//        map.put("maplist", listMap);
+        map.put("businessNumber", "1910221439386584asdf");
+        map.put("checkNumber", "JS20191128001");
+        map.put("sellerUnit", "XXXX科技有限公司");
+        map.put("buyerUnit", "XXXX集团有限责任公司");
+
+
+        List<Map<String, String>> listMap = new ArrayList<>();
+        int totalCount=0;
+        BigDecimal allMoney=new BigDecimal(0);
+
+        for (int i = 0; i < 4; i++) {
+            Map<String, String> lm = new HashMap<String, String>();
+            lm.put("category", "环保" + i);
+            lm.put("sku", "AT000" + i);
+            lm.put("productName", "testname"+i);
+            lm.put("unit", "条");
+            int count=RandomUtil.randomInt(1000,9999);
+            totalCount+=count;
+            lm.put("count", count+"");
+            int price=RandomUtil.randomInt(1000,9999);
+            lm.put("price", price+"");
+            lm.put("totalMoney", count*price + "");
+            allMoney.add(new BigDecimal(count*price));
+            lm.put("receiveDate",LocalDate.now().plusDays(RandomUtil.randomInt(1,5))+"");
+
+            listMap.add(lm);
+        }
+        map.put("maplist", listMap);
+        map.put("totalCount", totalCount+"");
+        map.put("allMoney", allMoney);
 
         Workbook workbook = ExcelExportUtil.exportExcel(params, map);
 
-
-
-        try {
-            ByteArrayOutputStream outPut = new ByteArrayOutputStream();
-            workbook.write(outPut);
-            byte[] bytes = outPut.toByteArray();
-            String fileName = "业务结算单.xlsx";
-            response.setCharacterEncoding("UTF-8");
-            DownloadWriter.writeToResponse(response, bytes, "application/vnd.ms-excel", fileName);
-        } catch (IOException e) {
-            throw new RuntimeException("下载业务结算单出错：" + e.getMessage());
+        if(workbook!=null){
+            try {
+                ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+                workbook.write(outPut);
+                byte[] bytes = outPut.toByteArray();
+                String fileName = "业务结算单test.xlsx";
+                response.setCharacterEncoding("UTF-8");
+                DownloadWriter.writeToResponse(response, bytes, "application/vnd.ms-excel", fileName);
+            } catch (IOException e) {
+                throw new RuntimeException("下载业务结算单出错：" + e.getMessage());
+            }
+        }else{
+            System.out.println("没找到");
         }
+
+
         return;
     }
 }
